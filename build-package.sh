76a6c09005495148204d6e2cf4ecd59b933c2d51
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-14 21:39:31 -0400

    gzip

---
+++ build-package.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+source ./config.properties
+
+log(){
+	echo `date`": "$1
+}
+
+
+date_string=`date +"%Y_%m_%d-%H_%M_%S"`
+random_string=`date | md5sum | head -c 20`
+reboot_count=`cat /home/root/REBOOT_COUNT`
+
+rm -fr $temp_data_dir/*
+mv $data_dir/* $temp_data_dir
+
+PACKAGE_NAME="${reboot_count}_${date_string}_${random_string}.zip"
+
+log "Building package "${PACKAGE_NAME}
+
+gzip $PACKAGE_NAME $temp_data_dir/*
