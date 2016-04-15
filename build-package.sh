89a43e03ac94ba5f03e9f36dc44d6dbba9d99e68
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-14 23:06:28 -0400

    fake data

--- build-package.sh
+++ build-package.sh
@@ -5,6 +5,9 @@ log(){
 	echo `date`": "$1
 }
 
+mkdir -p $data_packages_dir
+mkdir -p $data_dir
+mkdir -p $temp_data_dir
 
 date_string=`date +"%Y_%m_%d-%H_%M_%S"`
 random_string=`date | md5sum | head -c 20`
@@ -13,8 +16,8 @@ reboot_count=`cat /home/root/REBOOT_COUNT`
 rm -fr $temp_data_dir/*
 mv $data_dir/* $temp_data_dir
 
-PACKAGE_NAME="${reboot_count}_${date_string}_${random_string}.zip"
+PACKAGE_NAME="${reboot_count}_${date_string}_${random_string}.tar.gz"
 
 log "Building package "${PACKAGE_NAME}
 
-tar -zcvf $PACKAGE_NAME $temp_data_dir/*
+tar -zcvf "${data_packages_dir}/${PACKAGE_NAME}" $temp_data_dir/*
