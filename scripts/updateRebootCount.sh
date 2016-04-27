a09d4450bde921058481d48ea22b1a5d4624c28f
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-26 22:13:57 -0400

    fixed reboot count

--- scripts/updateRebootCount.sh
+++ scripts/updateRebootCount.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source ./config.txt
-count=`cat ${reboot_cout_file}`
+count=`cat ${reboot_count_file}`
 count=$((count+1))
-echo ${count} > ${reboot_cout_file}
+echo ${count} > ${reboot_count_file}
 echo "Incremented REBOOT_COUNT="${count}
