92fd1891d4e75502f4d5835b2bf9356d35013eb2
Pawel Raszewski <pawel.raszewski@teamaol.com>
2016-04-26 22:27:02 -0400

    dev setup

--- scripts/devSetup.sh
+++ scripts/devSetup.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-systemctl disable unicef-monitoring-daemon
 systemctl stop unicef-monitoring-daemon
+systemctl disable unicef-monitoring-daemon
+
+systemctl stop unicef-downloader
+systemctl disable unicef-downloader
+
 systemctl enable edison_config
 systemctl start edison_config
+
 systemctl enable wpa_supplicant
 systemctl start wpa_supplicant
+
 ifconfig wlan0 up
