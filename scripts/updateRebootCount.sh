a20f3134d44b4a34d1930b1217d9f31e5fa6cf39
Pawel Raszewski <raszewski@gmail.com>
2016-05-17 01:15:51 -0400

    create data dirs in rebootCout script

--- scripts/updateRebootCount.sh
+++ scripts/updateRebootCount.sh
@@ -4,3 +4,7 @@ count=`cat ${reboot_count_file}`
 count=$((count+1))
 echo ${count} > ${reboot_count_file}
 echo "Incremented REBOOT_COUNT="${count}
+echo "Creating DATA dirs if don't exist"
+mkdir -p $data_packages_dir
+mkdir -p $data_packages_dir/logs
+mkdir -p $data_dir
