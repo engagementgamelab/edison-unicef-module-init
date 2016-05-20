998d89894099f01921821eac7080728a7deff292
Pawel Raszewski <raszewski@gmail.com>
2016-05-19 21:36:16 -0400

    temp prod setup

--- scripts/prodSetup.sh
+++ scripts/prodSetup.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
+echo "updating downloader app..."
 cd /apps/downloader
 git pull
 npm install
+echo "updating monitoring app..."
 cd /apps/monitoring
 git pull
 npm install
 
+echo "enabling production services..."
 systemctl enable unicef-monitoring-daemon
 systemctl enable unicef-downloader
 systemctl disable edison_config
--- scripts/prodSetupTemporary.sh
+++ scripts/prodSetupTemporary.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
+echo "updating downloader app..."
 cd /apps/downloader
 git pull
 npm install
+echo "updating monitoring app..."
 cd /apps/monitoring
 git pull
 npm install
 
+echo "enabling production services..."
 systemctl start unicef-monitoring-daemon
 systemctl start unicef-downloader
 systemctl stop edison_config
 systemctl stop wpa_supplicant
 ifconfig wlan0 down
+echo "done"
+echo "you can revert it with devSetup.sh"
