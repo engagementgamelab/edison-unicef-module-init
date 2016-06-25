0e89cf7930defff2a14f8313f90b965de6a162b8
tomasair <tom@teporare.com>
2016-06-24 21:23:34 -0400

    cleanup inappropriate network down

--- scripts/prodSetupNoUpdate.sh
+++ scripts/prodSetupNoUpdate.sh
@@ -3,9 +3,7 @@ echo "enabling production services..."
 systemctl start unicef-monitoring-daemon
 systemctl start unicef-downloader
 systemctl stop edison_config
-systemctl stop wpa_supplicant
 systemctl stop pulseaudio
 systemctl stop redis
-ifconfig wlan0 down
 echo "done"
 echo "you can revert it with devSetup.sh"
